04fbf8c86df1755a70b6adc667573d2b4e8cde95
Ivan Dolhosheia <idolhosheia@gmail.com>
2024-02-08 15:56:48 +0200

    Add sout "HelloWorld 3.0"

--- src/main/java/com/homework/homework01/HelloWorld.java
+++ src/main/java/com/homework/homework01/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("HelloWorld");
         System.out.println("HelloWorld 2.0");
+        System.out.println("HelloWorld 3.0");
     }
 }

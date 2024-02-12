93ed5ee3545a81d9763acdc1d6cf27a82e22bf73
Ivan Dolhosheia <idolhosheia@gmail.com>
2024-02-12 14:44:39 +0200

    Homework09 - RugbyArray add first team age counter

--- src/main/java/com/homework/homework09/RugbyArray.java
+++ src/main/java/com/homework/homework09/RugbyArray.java
@@ -1,4 +1,20 @@
 package com.homework.homework09;
 
+import java.util.Scanner;
+
 public class RugbyArray {
+    public static void main(String[] args) {
+        Scanner scanTeam1 = new Scanner(System.in);
+        System.out.println("Enter the name of the first Rugby team: ");
+        int scanTeamOne = scanTeam1.nextInt();
+        int sumAgeTeamOne = 0;
+        int maxPlayersTeamOne = 10;
+        for (int i = 1; i <= maxPlayersTeamOne; i++) {
+            System.out.println("Enter the age of the player " + i + " from the team " + scanTeamOne + ": ");
+            int fragPlayerTeam1 = scanTeam1.nextInt();
+            sumAgeTeamOne += fragPlayerTeam1;
+        }
+        double avgAgeTeamOne = (double) sumAgeTeamOne / maxPlayersTeamOne;
+        System.out.println(avgAgeTeamOne);
+    }
 }

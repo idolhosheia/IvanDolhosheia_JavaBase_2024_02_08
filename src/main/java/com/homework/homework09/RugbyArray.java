9cd6f7be0e2850fa38df0648d32a398f34f96c4f
Ivan Dolhosheia <idolhosheia@gmail.com>
2024-02-12 14:54:46 +0200

    add second team age counter modified names of variables modified sout for the first team's average age

--- src/main/java/com/homework/homework09/RugbyArray.java
+++ src/main/java/com/homework/homework09/RugbyArray.java
@@ -4,17 +4,30 @@ import java.util.Scanner;
 
 public class RugbyArray {
     public static void main(String[] args) {
-        Scanner scanTeam1 = new Scanner(System.in);
+        Scanner scanTeamOneName = new Scanner(System.in);
         System.out.println("Enter the name of the first Rugby team: ");
-        int scanTeamOne = scanTeam1.nextInt();
+        String scanTeamOne = scanTeamOneName.nextLine();
         int sumAgeTeamOne = 0;
-        int maxPlayersTeamOne = 10;
+        int maxPlayersTeamOne = 25;
         for (int i = 1; i <= maxPlayersTeamOne; i++) {
             System.out.println("Enter the age of the player " + i + " from the team " + scanTeamOne + ": ");
-            int fragPlayerTeam1 = scanTeam1.nextInt();
-            sumAgeTeamOne += fragPlayerTeam1;
+            int agePlayerTeamOne = scanTeamOneName.nextInt();
+            sumAgeTeamOne += agePlayerTeamOne;
         }
         double avgAgeTeamOne = (double) sumAgeTeamOne / maxPlayersTeamOne;
-        System.out.println(avgAgeTeamOne);
+        System.out.println("The average age of the team " + scanTeamOne + " is: " + avgAgeTeamOne);
+
+        Scanner scanTeamTwoName = new Scanner(System.in);
+        System.out.println("Enter the name of the second Rugby team: ");
+        String scanTeamTwo = scanTeamTwoName.nextLine();
+        int sumAgeTeamTwo = 0;
+        int maxPlayersTeamTwo = 25;
+        for (int i = 1; i <= maxPlayersTeamTwo; i++) {
+            System.out.println("Enter the age of the player " + i + " from the team " + scanTeamTwo + ": ");
+            int agePlayerTeamTwo = scanTeamTwoName.nextInt();
+            sumAgeTeamTwo += agePlayerTeamTwo;
+        }
+        double avgAgeTeamTwo = (double) sumAgeTeamTwo / maxPlayersTeamTwo;
+        System.out.println("The average age of the team " + scanTeamTwo + " is: " + avgAgeTeamTwo);
     }
 }

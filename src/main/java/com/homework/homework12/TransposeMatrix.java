4b1b3d1f44cb57e6683eb225b35ef43195d1c952
Ivan Dolhosheia <idolhosheia@gmail.com>
2024-02-15 15:57:56 +0200

    Created (M x N) array and after transposed to (N x M) array. The variables M and N are set by scanner.

--- src/main/java/com/homework/homework12/TransposeMatrix.java
+++ src/main/java/com/homework/homework12/TransposeMatrix.java
@@ -4,14 +4,38 @@ import java.util.Scanner;
 
 public class TransposeMatrix {
     public static void main(String[] args) {
-        Scanner scanMatrix = new Scanner(System.in);
+        Scanner scanArray = new Scanner(System.in);
 
-        System.out.println("Enter the size of M axis: ");
-        int scanValueM = scanMatrix.nextInt();
-        System.out.println("The size of M axis is: " + scanValueM);
+        System.out.println("Enter the size of rows (M): ");
+        int scanRows = scanArray.nextInt();
+        //System.out.println("The size of M axis is: " + scanRows);
 
-        System.out.println("Enter the size of N axis: ");
-        int scanValueN = scanMatrix.nextInt();
-        System.out.println("The size of N axis is: " + scanValueN);
+        System.out.println("Enter the size of columns (N): ");
+        int scanColumns = scanArray.nextInt();
+        //System.out.println("The size of N axis is: " + scanColumns);
+
+        int[][] arrayOriginal = new int[scanRows][scanColumns];
+        int[][] arrayTranspose = new int[scanColumns][scanRows];
+
+        System.out.println("Original array (M x N): ");
+        displayArray(arrayOriginal);
+
+        System.out.println("Transpose array (N x M): ");
+        displayArray(arrayTranspose);
+
+        scanArray.close();
+    }
+
+    private static void displayArray(int[][] array) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                System.out.print(array[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
+
+// Транспонуйте матрицю M x N.
+// Розміри M та N задаються з консолі.
+// Після введення повинні автоматично створюватися два масиви розміром M x N перший та N x M другий.

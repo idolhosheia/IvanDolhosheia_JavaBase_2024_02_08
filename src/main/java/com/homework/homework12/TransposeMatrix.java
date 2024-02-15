238be45a77609142e9e40872ad2c3dce7e0a2ad7
Ivan Dolhosheia <idolhosheia@gmail.com>
2024-02-15 04:17:11 +0200

    The variables M and N of matrix are set using console
    // Транспонуйте матрицю M x N.
    // Розміри M та N задаються з консолі.
    // Після введення повинні автоматично створюватися два масиви розміром M x N перший та N x M другий.

--- src/main/java/com/homework/homework12/TransposeMatrix.java
+++ src/main/java/com/homework/homework12/TransposeMatrix.java
@@ -4,5 +4,14 @@ import java.util.Scanner;
 
 public class TransposeMatrix {
     public static void main(String[] args) {
+        Scanner scanMatrix = new Scanner(System.in);
+
+        System.out.println("Enter the size of M axis: ");
+        int scanValueM = scanMatrix.nextInt();
+        System.out.println("The size of M axis is: " + scanValueM);
+
+        System.out.println("Enter the size of N axis: ");
+        int scanValueN = scanMatrix.nextInt();
+        System.out.println("The size of N axis is: " + scanValueN);
     }
 }

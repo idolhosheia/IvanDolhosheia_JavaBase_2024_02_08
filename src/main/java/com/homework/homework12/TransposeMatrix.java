49e2ec68ed643372c6140c9cb7f675880b9f731e
Ivan Dolhosheia <idolhosheia@gmail.com>
2024-02-18 14:13:34 +0200

    transpose matrix done

--- src/main/java/com/homework/homework12/TransposeMatrix.java
+++ src/main/java/com/homework/homework12/TransposeMatrix.java
@@ -1,41 +1,42 @@
 package com.homework.homework12;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class TransposeMatrix {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Enter the size of rows (M): ");
+        System.out.println("Enter the number of rows (M): ");
         int rows = scanner.nextInt();
-        //System.out.println("The size of M axis is: " + rows);
 
-        System.out.println("Enter the size of columns (N): ");
+        System.out.println("Enter the number of columns (N): ");
         int columns = scanner.nextInt();
-        //System.out.println("The size of N axis is: " + columns);
 
-        int[][] arrayOriginal = new int[rows][columns];
-        int[][] arrayTranspose = new int[columns][rows];
+        int matrix[][] = new int[rows][columns];
 
-        System.out.println("Original array (M x N): ");
-        displayArray(arrayOriginal);
+        System.out.println("Enter the numbers for the matrix:");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                matrix[i][j] = scanner.nextInt();
+                System.out.print("");
+            }
+        }
 
-        System.out.println("Transpose array (N x M): ");
-        displayArray(arrayTranspose);
+        System.out.println("The original matrix: ");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
 
-        scanner.close();
-    }
-
-    private static void displayArray(int[][] array) {
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array[i].length; j++) {
-                System.out.print(array[i][j] + " ");
+        System.out.println("The transpose matrix: ");
+        for (int i = 0; i < columns; i++) {
+            for (int j = 0; j < rows; j++) {
+                System.out.print(matrix[j][i] + " ");
             }
             System.out.println();
         }
     }
 }
-
-// Транспонуйте матрицю M x N.
-// Розміри M та N задаються з консолі.
-// Після введення повинні автоматично створюватися два масиви розміром M x N перший та N x M другий.
